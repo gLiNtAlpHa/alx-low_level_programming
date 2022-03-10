f8659f5b2bf184df04cb203fb65d93848175073f
gLiNtAlpHa <90543690+gLiNtAlpHa@users.noreply.github.com>
2022-03-11 00:45:25 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,4 @@
 *main - Programming is like building a multilingual puzzle
 *Return: 0 if properly executed, else non-zero
 */
- int main(void)
- {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
- }
+int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0);}
