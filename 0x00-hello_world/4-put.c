d31756faaa1f579ed87e10e766b1778936aeb966
gLiNtAlpHa <90543690+gLiNtAlpHa@users.noreply.github.com>
2022-03-10 23:36:53 +0100

    Create 4-put.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main - Programming is like building a multilingual puzzle
+*Return: 0 if properly executed, else non-zero
+*/
+int main(void)
+{
+  put("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
