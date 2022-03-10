11bd7ab168ef17065906abafa69957dff2c797c0
gLiNtAlpHa <90543690+gLiNtAlpHa@users.noreply.github.com>
2022-03-10 23:41:59 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main - to print (with proper grammar, but the outcome is a piece of art)
+*return 0, if executed properly,else non-zero
+**/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
