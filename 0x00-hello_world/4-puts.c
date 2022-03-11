05d3be5edaa306ff634ae1d7364d96b2a87c2110
gLiNtAlpHa <sesaalolu@gmail.com>
2022-03-10 23:26:19 -0800

    edit by Aleshtech

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - Programming is like building a multilingual puzzle
-*Return: 0 if properly executed, else non-zero
+* main - Programming is like building a multilingual puzzle
+* Return: 0 if properly executed, else non-zero
 */
 int main(void)
 {
